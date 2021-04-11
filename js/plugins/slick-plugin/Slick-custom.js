0a2a01c138242aea70fc77ece847f91a6525079d
ArnobMahmud <arnob.tech.me@gmail.com>
2021-04-11 20:39:32 +0600

    new plugin init

---
+++ js/plugins/slick-plugin/Slick-custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".gallery").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+  });
+});
