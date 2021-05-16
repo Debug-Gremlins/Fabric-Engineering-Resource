78e5e457364dba6e05b6a613b60277027d1024e8
ArnobMahmud <arnob.tech.me@gmail.com>
2021-05-16 17:23:30 +0600

    update

--- src/components/includes/Footer.js
+++ src/components/includes/Footer.js
@@ -1,96 +1,96 @@
-import React from 'react'
-import Logo from '../../assets/icon.png'
+import React from "react";
+import Logo from "../../assets/icon.png";
 import { Link } from "react-router-dom";
-import Footbg from '../../assets/footer_bg.png';
+import Footbg from "../../assets/footer_bg.png";
 
 function Footer() {
-    return (
-      <div>
-        <footer data-aos="fade-up" data-aos-duration={3000}>
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-6">
-                <ul>
-                  <li>
-                    <span>Planned and Developed by :</span>
-                    <h3>Arnob Mahmud</h3>
-                  </li>
-                  <li>
-                    <span>Our Departmental Group : </span>
-                    <h3>
-                      <a
-                        href="https://www.facebook.com/groups/fabricmanufacturingengineering46/"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Fabric Engineering 46
-                      </a>
-                    </h3>
-                  </li>
-                  <li>
-                    {/* <a href="#home" target="_blank"></a> */}
-                    <Link to="/">
-                      <img className="rotating" src={Logo} alt="" srcSet />
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-lg-6">
-                <ul>
-                  <li>
-                    <h4>Suggest Changes to Update Next Version!</h4>
-                  </li>
-                  <li>
-                    <h5>Go to Individual Section</h5>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/bce">
-                      BCE
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/chemistry">
-                      Chemistry
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/math">
-                      Mathematics
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/ntf">
-                      NTF
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" to="/physics">
-                      Physics
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+  return (
+    <div>
+      <footer data-aos="fade-up" data-aos-duration={3000}>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6">
+              <ul>
+                <li>
+                  <span>Planned and Developed by :</span>
+                  <h3>Arnob Mahmud</h3>
+                </li>
+                <li>
+                  <span>Our Departmental Group : </span>
+                  <h3>
+                    <a
+                      href="https://www.facebook.com/groups/fabricmanufacturingengineering46/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Fabric Engineering 46
+                    </a>
+                  </h3>
+                </li>
+                <li>
+                  {/* <a href="#home" target="_blank"></a> */}
+                  <Link to="/">
+                    <img className="rotating" src={Logo} alt="" srcSet />
+                  </Link>
+                </li>
+              </ul>
             </div>
-            <div className="cpy-rgt">
-              Copyright ©2021{" "}
-              <span>
-                <Link to="/">Fabric Engineering 46</Link>
-              </span>{" "}
-              All Rights Reserved
-            </div>
-            <div className="overlaybttn">
-              <a id="bottombce" href="#top">
-                <i className="fa fa-arrow-circle-up" aria-hidden="true" />
-                <span>Top</span>
-              </a>
-            </div>
-            <div className="foot_bg">
-              <img src={Footbg} alt="footbg" srcSet />
+            <div className="col-lg-6">
+              <ul>
+                <li>
+                  <h4>Suggest Changes to Update Next Version!</h4>
+                </li>
+                <li>
+                  <h5>Go to Individual Section</h5>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/bce">
+                    BCE
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/chemistry">
+                    Chemistry
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/math">
+                    Mathematics
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/ntf">
+                    NTF
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/physics">
+                    Physics
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
-        </footer>
-      </div>
-    );
+          <div className="cpy-rgt">
+            Copyright ©2021{" "}
+            <span>
+              <Link to="/">Fabric Engineering 46</Link>
+            </span>{" "}
+            All Rights Reserved
+          </div>
+          <div className="overlaybttn">
+            <a id="bottombce" href="#top">
+              <i className="fa fa-arrow-circle-up" aria-hidden="true" />
+              <span>Top</span>
+            </a>
+          </div>
+          <div className="foot_bg">
+            <img src={Footbg} alt="footbg" srcSet />
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/includes/HomeNav.js
+++ src/components/includes/HomeNav.js
@@ -145,6 +145,17 @@ class HomeNavPart extends Component {
                 </div>
               </div>
             </div>
+            <svg
+              className="wave"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1440 320"
+            >
+              <path
+                fill="#FFD2DA"
+                fill-opacity="1"
+                d="M0,128L80,154.7C160,181,320,235,480,245.3C640,256,800,224,960,208C1120,192,1280,192,1360,192L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+              ></path>
+            </svg>
           </div>
         </div>
       </div>
