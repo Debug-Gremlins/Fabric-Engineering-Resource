00dde44ae2efbd63d240aaa167d840b4b0dbff8a
ArnobMahmud <arnob.tech.me@gmail.com>
2021-05-26 14:50:18 +0600

    update

--- src/components/Customs/CouterComp.js
+++ src/components/Customs/CouterComp.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
-
+import { IconContext } from "react-icons";
 class CouterComp extends Component {
   render() {
     return (
       <div>
         <div className="card">
+          <IconContext.Provider
+            className="icons"
+            value={{ size: "60", color: "#15202b" }}
+          >
+            {this.props.icons}
+          </IconContext.Provider>
           <h1>{this.props.title}</h1>
-          <h2 className="count">{this.props.num}</h2>
           <p>{this.props.paragraph}</p>
         </div>
       </div>
--- src/components/widgets/CounterUp.js
+++ src/components/widgets/CounterUp.js
@@ -1,32 +1,35 @@
 import React from "react";
 import CustomCount from "../customs/CouterComp";
-
+import { BiShieldAlt } from "react-icons/bi";
+import { FiSlack } from "react-icons/fi";
+import { ImBook } from "react-icons/im";
 function CounterUp() {
   return (
     <div>
-      <section
-        className="counter-area"
-      >
+      <section className="counter-area">
         <div className="container">
-          <div className="row justify-content-center text-center">
-            <div className="col-lg-4 col-md-4 col-sm-3 single-class">
-              <CustomCount title="BCE" num="20" paragraph="Lectures" />
+          <div className="row justify-content-center text-center align-items-center">
+            <div className="col-lg-3 col-md-4 col-sm-8 single-class">
+              <CustomCount
+                icons={<BiShieldAlt />}
+                title="Secured Development"
+                paragraph="Developed by JavaScript framework ReactJS."
+              />
             </div>
 
-            <div className="col-lg-4 col-md-4 col-sm-3 single-class">
-              <CustomCount title="Chemistry" num="30" paragraph="Lectures" />
+            <div className="col-lg-3 col-md-4 col-sm-8 single-class">
+              <CustomCount
+                icons={<ImBook />}
+                title="Coursewise Resources"
+                paragraph="Online class lectures provided by coursewise segments!"
+              />
             </div>
-
-            <div className="col-lg-4 col-md-4 col-sm-3 single-class">
-              <CustomCount title="MATH" num="20" paragraph="Lectures" />
-            </div>
-
-            <div className="col-lg-4 col-md-4 col-sm-3 single-class">
-              <CustomCount title="NTF" num="20" paragraph="Lectures" />
-            </div>
-
-            <div className="col-lg-4 col-md-4 col-sm-3 single-class">
-              <CustomCount title="Physics" num="20" paragraph="Lectures" />
+            <div className="col-lg-3 col-md-4 col-sm-8 single-class">
+              <CustomCount
+                icons={<FiSlack />}
+                title="Regular Monitorization"
+                paragraph="Keeping resources and pdf stuffs update weekly."
+              />
             </div>
           </div>
         </div>
