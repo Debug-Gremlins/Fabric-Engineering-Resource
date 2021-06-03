3b7a699691cbad0bedf7ac865aa99d1c6e1dc553
ArnobMahmud <arnob.tech.me@gmail.com>
2021-06-03 12:05:54 +0600

    animation added

--- src/components/customs/UpperBanner.js
+++ src/components/customs/UpperBanner.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 import { FcBinoculars } from "react-icons/fc";
 import { IconContext } from "react-icons";
 import bannerImg from "../../assets/topbnrbg2.png";
+import balloon from "../../assets/balloon.png";
+import cloud from "../../assets/cloud.png";
+import cloud2 from "../../assets/cloud2.png";
 
 class UpperBanner extends Component {
   render() {
@@ -10,6 +13,9 @@ class UpperBanner extends Component {
       <div>
         <TopBannerArea>
           <div className="top-bnr-overlay">
+            <img src={balloon}  alt="balloon" className="balloon" srcSet />
+            <img src={cloud} alt="cloud" className="balloncloudfade" srcSet />
+            <img src={cloud2} alt="cloud" className="balloncloudfade2" srcSet />
             <div className="home-shield row text-center justify-content-between">
               <div className="col-lg-6 col-md-6 col-sm-12">
                 <a
@@ -96,6 +102,136 @@ const TopBannerArea = styled.div`
     right: 20px;
   }
 
+  .balloon {
+    animation: balloonup 13s, balloonrotate 10s;
+    animation-direction: alternate;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
+    height: auto;
+    max-width: 150px;
+    position: absolute;
+    right: 18%;
+    top: 10%;
+    width: 6%;
+    z-index: 2;
+  }
+  @keyframes balloonup {
+    0% {
+      top: 30%;
+    }
+    25% {
+      top: 25%;
+    }
+    50% {
+      top: 20%;
+    }
+    75% {
+      top: 25%;
+    }
+    100% {
+      top: 30%;
+    }
+  }
+  @keyframes balloonrotate {
+    0% {
+      transform: rotate(-3deg);
+    }
+    20% {
+      transform: rotate(4deg);
+    }
+    45% {
+      transform: rotate(-4deg);
+    }
+    70% {
+      transform: rotate(4deg);
+    }
+    100% {
+      transform: rotate(-2deg);
+    }
+  }
+
+  .balloncloudfade {
+    animation: bpcloudfade1 27s, bpslide1 27s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    height: auto;
+    max-width: 300px;
+    position: absolute;
+    right: 15%;
+    top: 20.5%;
+    width: 16%;
+    z-index: 1;
+  }
+  @keyframes bpcloudfade1 {
+    0% {
+      opacity: 0;
+    }
+    25% {
+      opacity: 1;
+    }
+    35% {
+      opacity: 1;
+    }
+    90% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+  @keyframes bpslide1 {
+    0% {
+      right: 25.5%;
+    }
+    25% {
+      right: 20.5%;
+    }
+    100% {
+      right: 2%;
+    }
+  }
+
+  .balloncloudfade2 {
+    animation: bpcloudfade2 27s, bpslide2 27s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    height: auto;
+    max-width: 300px;
+    position: absolute;
+    right: 15%;
+    top: 40.5%;
+    width: 16%;
+    z-index: 1;
+  }
+  @keyframes bpcloudfade2 {
+    0% {
+      opacity: 0;
+    }
+    25% {
+      opacity: 1;
+    }
+    35% {
+      opacity: 1;
+    }
+    90% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+  @keyframes bpslide2 {
+    0% {
+      right: 38.5%;
+    }
+    25% {
+      right: 28.5%;
+    }
+    100% {
+      right: 9%;
+    }
+  }
+
   @media (max-width: 991px) {
     .top-over-details-part {
       width: 70%;
@@ -109,11 +245,104 @@ const TopBannerArea = styled.div`
   }
 
   @media (max-width: 767px) {
+    .balloncloudfade2 {
+      display: none;
+    }
     .home-shield {
       position: absolute;
       bottom: 20px;
       right: 10px;
     }
+
+    .balloon {
+      animation: balloonup 13s, balloonrotate 10s;
+      animation-direction: alternate;
+      animation-iteration-count: infinite;
+      animation-timing-function: ease-in-out;
+      height: auto;
+      max-width: 150px;
+      position: absolute;
+      right: 5%;
+      top: 10%;
+      width: 9%;
+      z-index: 2;
+    }
+    @keyframes balloonup {
+      0% {
+        top: 30%;
+      }
+      25% {
+        top: 25%;
+      }
+      50% {
+        top: 20%;
+      }
+      75% {
+        top: 25%;
+      }
+      100% {
+        top: 30%;
+      }
+    }
+    @keyframes balloonrotate {
+      0% {
+        transform: rotate(-3deg);
+      }
+      20% {
+        transform: rotate(4deg);
+      }
+      45% {
+        transform: rotate(-4deg);
+      }
+      70% {
+        transform: rotate(4deg);
+      }
+      100% {
+        transform: rotate(-2deg);
+      }
+    }
+
+    .balloncloudfade {
+      animation: bpcloudfade1 27s, bpslide1 27s;
+      animation-iteration-count: infinite;
+      animation-timing-function: linear;
+      height: auto;
+      max-width: 300px;
+      position: absolute;
+      right: 15%;
+      top: 21.5%;
+      width: 16%;
+      z-index: 1;
+    }
+    @keyframes bpcloudfade1 {
+      0% {
+        opacity: 0;
+      }
+      25% {
+        opacity: 1;
+      }
+      35% {
+        opacity: 1;
+      }
+      90% {
+        opacity: 1;
+      }
+      100% {
+        opacity: 0;
+      }
+    }
+    @keyframes bpslide1 {
+      0% {
+        right: 20.5%;
+      }
+      25% {
+        right: 13.5%;
+      }
+      100% {
+        right: 2%;
+      }
+    }
+
     .top-over-details-part {
       width: 100%;
       padding: 180px 20px 120px;
